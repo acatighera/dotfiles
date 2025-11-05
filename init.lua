a807845fc1d289c77c04b52b30dddeab87e912f9
Alexandru Catighera <alex@ghosty.app>
2025-11-05 11:00:58 -0500

    remove bubblegum color scheme

--- init.lua
+++ init.lua
@@ -148,8 +148,8 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], {desc='Terminal -> Normal'})
 
-vim.o.background = "dark"
-vim.cmd.colorscheme "bubblegum-256-dark"
+-- vim.o.background = "dark"
+-- vim.cmd.colorscheme "bubblegum-256-dark"
 
 local transparency_groups = {
   "Normal",
